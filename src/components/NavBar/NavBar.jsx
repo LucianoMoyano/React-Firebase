b1aee79feeecbef6ba7293e7e369645f7363813d
Luciano Moyano <lucianoarielmoyano@outlook.com>
2023-06-16 21:52:07 -0300

    minimo cambio del perfil

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import { useAuth } from "../../context/AuthContext";
 import "./nav.css";
 
 export const NavBar = () => {
+  const { user } = useAuth();
+
   return (
     <div>
       <Link to="/home">Home</Link>
-      <Link to="/profile">Mi perfil</Link>
+      {user && <Link to="/profile">Mi perfil</Link>}
       <Link to="/login">Iniciar Sesión</Link>
     </div>
   );
